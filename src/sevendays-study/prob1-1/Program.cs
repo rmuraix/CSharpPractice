4e3211f6dab071bc137c3d94dfa3e663a1f262a2
rmuraix <35632215+rmuraix@users.noreply.github.com>
2022-08-04 10:56:58 +0900

    complete1-1

--- src/sevendays-study/prob1-1/Program.cs
+++ src/sevendays-study/prob1-1/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace prob1_1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("I'm @rmuraix!");
         }
     }
 }
