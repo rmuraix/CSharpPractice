0e05789257711f74a7841c96a02d28b2216032ba
rmuraix <35632215+rmuraix@users.noreply.github.com>
2022-08-05 15:42:14 +0900

    add 2-4

---
+++ src/sevendays-study/prob2-4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace prob2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? str1;
+
+            Console.Write("文字列を入力：");
+            str1 = Console.ReadLine();
+            
+            Console.Write("入力された文字列：");
+            Console.WriteLine(str1);
+        }
+    }
+}
