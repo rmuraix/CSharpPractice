0cb7031e5509b753535ce35658e1009e5a8ac8d8
rmuraix <35632215+rmuraix@users.noreply.github.com>
2022-09-06 18:08:06 +0900

    add 3-5

---
+++ src/sevendays-study/03/prob3-5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace prob3_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数値を入力:");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num > 30 || num <= 70)
+            {
+                Console.WriteLine("30より大きく70以下です");
+            }
+        }
+    }
+}
