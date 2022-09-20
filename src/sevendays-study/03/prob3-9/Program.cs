6c3400ca6688dbb9131de13f5ee229f1728aeec2
rmuraix <35632215+rmuraix@users.noreply.github.com>
2022-09-20 21:41:47 +0900

    add 3-9

---
+++ src/sevendays-study/03/prob3-9/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace prob3_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("文字列を入力:");
+            string inputStr = Console.ReadLine();
+
+            if (inputStr == "abc")
+            {
+                Console.WriteLine("abcです。");
+            }
+            else
+            {
+                Console.WriteLine("abcではありません。");
+            }
+        }
+    }
+}
