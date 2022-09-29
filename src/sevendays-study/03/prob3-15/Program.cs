2deb759c57886fa1f03e7b4df5c9d1b9c531badd
rmuraix <35632215+rmuraix@users.noreply.github.com>
2022-09-29 15:24:58 +0900

    Add 3-15

---
+++ src/sevendays-study/03/prob3-15/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace prob3_15
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("点数(1-100):");
+            int num = int.Parse(Console.ReadLine());
+
+            switch (num)
+            {
+                case > 100 or < 1:
+                    Console.WriteLine("範囲外です");
+                    break;
+                case >= 80:
+                    Console.WriteLine("優");
+                    break;
+                case >= 70:
+                    Console.WriteLine("良");
+                    break;
+                case >= 60:
+                    Console.WriteLine("可");
+                    break;
+                default:
+                    Console.WriteLine("不可");
+                    break;
+            }
+        }
+    }
+}
