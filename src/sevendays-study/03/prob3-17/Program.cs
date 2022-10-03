05b1312a3a2581dbdabc8e24ea942374c890c894
rmuraix <35632215+rmuraix@users.noreply.github.com>
2022-10-03 12:02:07 +0900

    Add 3-17

---
+++ src/sevendays-study/03/prob3-17/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace prob3_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数を入力してください(1-100):");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num is > 100 or < 1)
+            {
+                Console.WriteLine("範囲外の値です。");
+            }
+            else if (num % 2 == 0)
+            {
+                Console.WriteLine(num <= 50 ? "50以下の偶数です。" : "偶数です。");
+            }
+            else if (num <= 50)
+            {
+                Console.WriteLine("50以下です。");
+            }
+        }
+    }
+}
